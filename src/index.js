57fac182a8a94e3dff0386ba920e504ab05233fb
Rami1x <rami.ayourama@gmail.com>
2024-10-08 15:27:45 -0600

    prt3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-function Football() {
-  const shoot = () => {
-    alert("Great Shot!");
-  }
-
+export default function App() {
   return (
-    <button onClick={shoot}>Take thw Shot!</button>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Football />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />)
